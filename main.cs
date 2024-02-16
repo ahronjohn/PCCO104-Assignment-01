05b87ee259b5c9b0d65da10d74fc24e865533482
ahronjohn <158656890+ahronjohn@users.noreply.github.com>
2024-02-16 13:43:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,8 @@
+using System;
+
+    class Program {
+      public static void Main (string[] args) {
+        
+        Console.Write("Enter the denomination (e.g., 1, 5, 10, 100, etc.): ");
+        string input = Console.ReadLine();
+        
